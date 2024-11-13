9cbe279ac11a83d003e758010bd29c37a4067896
Patrick Höhn <hoehnp@gmx.de>
2024-11-13 20:42:08 +0100

    added some test scripts

---
+++ docker/install_fe40.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+source /home/foamuser/foam/foam-extend-4.0/etc/bashrc
+
+cd /home/foamuser/precice/
+git config --global --add safe.directory /home/foamuser/precice/./.git
+git clone . ../adapter
+cd ../adapter
+git checkout foam-extend-4.0 
+LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH ./Allwmake
