15d5019df4dfd77f8f2d3a96941d95a4d9a6c61a
Patrick Höhn <hoehnp@gmx.de>
2024-11-13 20:42:08 +0100

    added some test scripts

---
+++ docker/test_fe50.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+podman run --rm --name precice-fe50 --volume $(pwd)/..:/home/foamuser/precice:copy  ghcr.io/hoehnp/openfoam-adapter:precice-foam-extend-5.0 bash -c /home/foamuser/precice/docker/install_fe50.sh 
