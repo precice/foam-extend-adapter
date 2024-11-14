f9d38dd15ba0a155f60a86198d6625d0079f0c15
Patrick Höhn <hoehnp@gmx.de>
2024-11-14 11:14:12 +0100

    verify compiler version

--- docker/install_fe40.sh
+++ docker/install_fe40.sh
@@ -6,5 +6,6 @@ cd /home/foamuser/precice/
 git config --global --add safe.directory /home/foamuser/precice/./.git
 git clone . ../adapter
 cd ../adapter
-git checkout foam-extend-4.0 
-LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH ./Allwmake
+git checkout foam-extend-4.0
+gcc --version
+#LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH ./Allwmake
