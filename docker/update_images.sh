7624a29d8ca1ee8f77b834a8398d30a66eccb226
Patrick Höhn <hoehnp@gmx.de>
2025-07-08 10:20:34 +0200

    updated docker images

---
+++ docker/update_images.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#for version in 4.0 4.1 5.0 dev
+for version in dev
+do
+	echo "building versioni fe-$version"
+	cd fe-$version
+	podman build -t=ghcr.io/hoehnp/openfoam-adapter:precice-foam-extend-$version . 
+	podman push ghcr.io/hoehnp/openfoam-adapter:precice-foam-extend-$version
+	cd ..
+done
