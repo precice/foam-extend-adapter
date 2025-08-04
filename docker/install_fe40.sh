c39447b77a9acb3c67046c494a4e0651164e3280
Patrick Höhn <patrick.hoehn@uni-goettingen.de>
2025-08-04 19:25:05 +0200

    added files to unify docker between branches

--- docker/install_fe40.sh
+++ docker/install_fe40.sh
@@ -6,5 +6,14 @@ cd /home/foamuser/precice/
 git config --global --add safe.directory /home/foamuser/precice/./.git
 git clone . ../adapter
 cd ../adapter
-git checkout foam-extend-4.0 
+git checkout foam-extend-4.0
 LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH ./Allwmake
+
+cd /home/foamuser/tutorials/quickstart
+rm -rf precice-run
+cd solid-cpp
+LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH cmake .
+LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH make
+./rigid-body-solver &
+cd ../fluid-foam-extend
+pimpleDyMFoam
