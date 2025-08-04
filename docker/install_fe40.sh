33a4876931463fbff01e722795d25d4f530225a8
Patrick Höhn <hoehnp@gmx.de>
2025-08-04 19:07:03 +0200

    updated files

--- docker/install_fe40.sh
+++ docker/install_fe40.sh
@@ -9,3 +9,11 @@ cd ../adapter
 git checkout foam-extend-4.0
 LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH ./Allwmake
 
+cd /home/foamuser/tutorials/quickstart
+rm -rf precice-run
+cd solid-cpp
+LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH cmake .
+LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH make
+./rigid-body-solver &
+cd ../fluid-foam-extend
+pimpleDyMFoam
