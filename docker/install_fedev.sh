ce6ea19eeefe0d3a64ae4640ee0fd1b8017e9b4d
Patrick Höhn <patrick.hoehn@uni-goettingen.de>
2025-08-05 21:08:36 +0200

    added dev tests

---
+++ docker/install_fedev.sh
@@ -0,0 +1,19 @@
+!/bin/bash 
+
+source /home/foamuser/foam/foam-extend-5.1/etc/bashrc
+
+cd /home/foamuser/precice/
+git config --global --add safe.directory /home/foamuser/precice/./.git
+git clone . ../adapter
+cd ../adapter
+git checkout foam-extend-dev
+./Allwmake
+
+cd /home/foamuser/tutorials/quickstart
+rm -rf precice-run
+cd solid-cpp
+cmake .
+make
+./rigid-body-solver &
+cd ../fluid-foam-extend
+pimpleDyMFoam
