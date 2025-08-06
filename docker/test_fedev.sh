cd5f7fc47be4346036c5aa10ce411556e2616f6a
Patrick Höhn <patrick.hoehn@uni-goettingen.de>
2025-08-06 18:55:51 +0200

    fix fedev

--- docker/test_fedev.sh
+++ docker/test_fedev.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-podman run --rm --name precice-fedev --volume $(pwd)/..:/home/foamuser/precice:copy  --volume $(pwd)/../../tutorials:/home/foamuser/tutorials:copy ghcr.io/hoehnp/openfoam-adapter:precice-foam-extend-dev bash -c /home/foamuser/precice/docker/install_fedev.sh 
+podman run --rm --name precice-fedev --volume $(pwd)/..:/home/foamuser/precice:copy  ghcr.io/hoehnp/openfoam-adapter:precice-foam-extend-dev bash -c /home/foamuser/precice/docker/install_fedev.sh 
