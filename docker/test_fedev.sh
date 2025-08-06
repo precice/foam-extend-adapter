bd7a8f72dad699dbffb969eddc2ec4c1ee949a2f
Patrick Höhn <patrick.hoehn@uni-goettingen.de>
2025-08-06 11:11:56 +0200

    add files for dev version of foam-extend

---
+++ docker/test_fedev.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+podman run --rm --name precice-fedev --volume $(pwd)/..:/home/foamuser/precice:copy  ghcr.io/hoehnp/openfoam-adapter:precice-foam-extend-dev bash -c /home/foamuser/precice/docker/install_fedev.sh 
