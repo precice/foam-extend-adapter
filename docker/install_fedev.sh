7b29a5aa558416cb7e15fa5d3673021ebdae04c7
Patrick Höhn <patrick.hoehn@uni-goettingen.de>
2025-08-08 11:45:31 +0200

    fix shell scripts linting

--- docker/install_fedev.sh
+++ docker/install_fedev.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 
 # shellcheck source=/dev/null
-source /home/foamuser/foam/foam-extend-4.1/etc/bashrc
+source /home/foamuser/foam/foam-extend-5.1/etc/bashrc
 
 cd /home/foamuser/precice/ || exit
 git config --global --add safe.directory /home/foamuser/precice/./.git
 git clone . ../adapter
 cd ../adapter || exit
-git checkout foam-extend-4.1
+git checkout foam-extend-dev
 ./Allwmake
 
 cd /home/foamuser/tutorials/quickstart || exit
@@ -15,7 +15,6 @@ rm -rf precice-run
 cd solid-cpp || exit
 cmake .
 make
-
 ./rigid_body_solver &
 cd ../fluid-foam-extend || exit
 blockMesh
