f7d38b4d2ee7556b3f7c60887623b01a59cc870f
Patrick Höhn <patrick.hoehn@uni-goettingen.de>
2025-08-08 12:37:15 +0200

    add diagnosis

--- docker/install_fe41.sh
+++ docker/install_fe41.sh
@@ -5,6 +5,7 @@ source /home/foamuser/foam/foam-extend-4.1/etc/bashrc
 
 cd /home/foamuser/precice/ || exit
 git config --global --add safe.directory /home/foamuser/precice/./.git
+ls -lahS ..
 git clone . ../adapter
 cd ../adapter || exit
 git checkout foam-extend-4.1
