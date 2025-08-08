d3f8a3165aeefea1b84f3648c99c6c177142318a
Patrick Höhn <patrick.hoehn@uni-goettingen.de>
2025-08-08 14:58:11 +0200

    added changes for foam-extend-4.0 now floating point exception

--- docker/update_images.sh
+++ docker/update_images.sh
@@ -4,8 +4,8 @@ for version in 4.0 4.1 5.0 dev
 #for version in 4.1
 do
 	echo "building versioni fe-$version"
-	cd fe-$version
-	podman build -t=ghcr.io/hoehnp/openfoam-adapter:precice-foam-extend-$version . 
+	cd fe-$version || exit
+	podman build --no-cache -t=ghcr.io/hoehnp/openfoam-adapter:precice-foam-extend-$version .
 	podman push ghcr.io/hoehnp/openfoam-adapter:precice-foam-extend-$version
 	cd ..
 done
